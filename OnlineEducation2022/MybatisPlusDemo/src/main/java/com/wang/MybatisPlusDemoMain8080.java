ecd497c583d5bcc9a7305131265e44af5982a0ec
reggou <2780828962@qq.com>
2022-11-05 16:11:14 +0800

    swagger

--- OnlineEducation2022/MybatisPlusDemo/src/main/java/com/wang/MybatisPlusDemoMain8080.java
+++ OnlineEducation2022/MybatisPlusDemo/src/main/java/com/wang/MybatisPlusDemoMain8080.java
@@ -3,6 +3,7 @@ package com.wang;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author wzzmm
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @MapperScan("com.wang.mapper")
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.wang.servicebase"})
 public class MybatisPlusDemoMain8080 {
     public static void main(String[] args) {
         SpringApplication.run(MybatisPlusDemoMain8080.class,args);
